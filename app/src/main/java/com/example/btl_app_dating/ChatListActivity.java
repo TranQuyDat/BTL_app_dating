d22c9664700b1249fa2d9ab65aeab6b997fa1531
MinhDuckk <minhducnguyen257@gmail.com>
2023-04-07 01:08:05 +0700

    update: hoan thanh profile user

--- app/src/main/java/com/example/btl_app_dating/ChatListActivity.java
+++ app/src/main/java/com/example/btl_app_dating/ChatListActivity.java
@@ -1,11 +1,16 @@
 package com.example.btl_app_dating;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -25,17 +30,64 @@ public class ChatListActivity extends AppCompatActivity {
     private RecyclerView rcvUser;
     private UserAdapter userAdapter;
 
+    private List<ChatMessage> list_chatobj= new ArrayList<>();
+
     private DatabaseReference db_conv = FirebaseDatabase.getInstance().getReference("conversations");
+
+    private long id =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chatlist);
 
         rcvUser= findViewById(R.id.rcv_user);
-        userAdapter= new UserAdapter(this);
+        userAdapter= new UserAdapter(list_chatobj)
+        rcvUser.setAdapter(userAdapter);
+        if(list_chatobj.size()<=0)
+            getdata_firebase();
 
         LinearLayoutManager linearLayoutManager= new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+    }
 
+    private void getdata_firebase(){
+        autoid();
+        db_conv.child("conv_0").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list_chatobj.clear();
+                if (snapshot.getChildrenCount()<=0) return;
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    ChatMessage chat = dataSnapshot.getValue(ChatMessage.class);
+                    list_chatobj.add(chat);
+                }
+                userAdapter.notifyDataSetChanged();
+                rcvUser.smoothScrollToPosition(userAdapter.getItemCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private int autoid(){
+        db_conv.child("conv_0").addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.getChildrenCount()!=0){
+                    id = snapshot.getChildrenCount();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return (int) id;
     }
 
 
